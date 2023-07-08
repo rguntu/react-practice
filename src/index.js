5acb75383c517165b25cb70a8c24ca27159def92
Raveendra Guntupalli <rguntupalli@liveperson.com>
2023-07-08 06:18:12 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App_useQuery_cache';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
+   
   </React.StrictMode>
 );
 
